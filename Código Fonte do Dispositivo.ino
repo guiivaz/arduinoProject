034026d2470b9bbabd5612e475beda2fb97cd828
Matheus Comar <57850267+MatheusComar@users.noreply.github.com>
2022-11-22 01:58:12 -0300

    Update Código Fonte do Dispositivo.ino

--- Código Fonte do Dispositivo.ino
+++ Código Fonte do Dispositivo.ino
@@ -2,85 +2,138 @@
 
 #include <idDHT11.h>
 
-int idDHT11pin = 2; //Digital pin for comunications
-int idDHT11intNumber = 0; //interrupt number (must be the one that use the previus defined pin (see table above)
+#include <idDHT11.h>
 
-//declaration
-void dht11_wrapper(); // must be declared before the lib initialization
+#include <idDHT11.h>
 
-// Lib instantiate
-idDHT11 DHT11(idDHT11pin,idDHT11intNumber,dht11_wrapper);
+
+
+
+#include <idDHT11.h>
+
+int idDHT11pin = 2;       
+int idDHT11intNumber = 0; 
+
+void dht11_wrapper();     
+void loopDHT();           
+
+idDHT11 DHT11(idDHT11pin, idDHT11intNumber, dht11_wrapper);   
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("idDHT11 Example program");
-  Serial.print("LIB version: ");
-  Serial.println(IDDHT11LIB_VERSION);
-  Serial.println("---------------");
+  Serial.println("Inicio do Sketch");
 }
-// This wrapper is in charge of calling 
-// mus be defined like this for the lib work
+
+
+float temperaturaC;
+float temperaturaF;
+float temperaturaK;
+float umidade;
+float dewPoint;
+float dewPointSlow;
+
+void loop()
+{
+
+  loopDHT(); 
+  
+  Serial.print("Temperatura Celcius: ");
+  Serial.println( temperaturaC );
+
+  Serial.print("Umidade Relativa: ");
+  Serial.println( umidade );
+
+  Serial.print("Ponto de Orvalho: ");
+  Serial.println( dewPoint );
+
+  Serial.println();
+}
+
 void dht11_wrapper() {
   DHT11.isrCallback();
 }
-void loop()
-{
-  Serial.print("\nRetrieving information from sensor: ");
-  Serial.print("Read sensor: ");
-  //delay(100);
-  DHT11.acquire();
-  while (DHT11.acquiring())
-    ;
-  int result = DHT11.getStatus();
-  switch (result)
-  {
-  case IDDHTLIB_OK: 
-    Serial.println("OK"); 
-    break;
-  case IDDHTLIB_ERROR_CHECKSUM: 
-    Serial.println("Error\n\r\tChecksum error"); 
-    break;
-  case IDDHTLIB_ERROR_ISR_TIMEOUT: 
-    Serial.println("Error\n\r\tISR Time out error"); 
-    break;
-  case IDDHTLIB_ERROR_RESPONSE_TIMEOUT: 
-    Serial.println("Error\n\r\tResponse time out error"); 
-    break;
-  case IDDHTLIB_ERROR_DATA_TIMEOUT: 
-    Serial.println("Error\n\r\tData time out error"); 
-    break;
-  case IDDHTLIB_ERROR_ACQUIRING: 
-    Serial.println("Error\n\r\tAcquiring"); 
-    break;
-  case IDDHTLIB_ERROR_DELTA: 
-    Serial.println("Error\n\r\tDelta time to small"); 
-    break;
-  case IDDHTLIB_ERROR_NOTSTARTED: 
-    Serial.println("Error\n\r\tNot started"); 
-    break;
-  default: 
-    Serial.println("Unknown error"); 
-    break;
+
+void loopDHT() {
+#define tempoLeitura 1000
+static unsigned long delayLeitura = millis() + tempoLeitura + 1;
+static bool request = false;
+
+  if ((millis() - delayLeitura) > tempoLeitura) { 
+      if (!request) {
+         DHT11.acquire(); 
+         request = true;
+      }
   }
-  Serial.print("Humidity (%): ");
-  Serial.println(DHT11.getHumidity(), 2);
 
-  Serial.print("Temperature (oC): ");
-  Serial.println(DHT11.getCelsius(), 2);
+  if (request && !DHT11.acquiring()) {
+    request = false;
 
-  Serial.print("Temperature (oF): ");
-  Serial.println(DHT11.getFahrenheit(), 2);
+    int result = DHT11.getStatus();
+      
+    switch (result)
+    {
+    case IDDHTLIB_OK: 
+        Serial.println("Leitura OK"); 
+        break;
+    case IDDHTLIB_ERROR_CHECKSUM: 
+        Serial.println("Erro\n\r\tErro Checksum"); 
+        break;
+    case IDDHTLIB_ERROR_ISR_TIMEOUT: 
+        Serial.println("Erro\n\r\tISR Time out"); 
+        break;
+    case IDDHTLIB_ERROR_RESPONSE_TIMEOUT: 
+        Serial.println("Erro\n\r\tResponse time out"); 
+        break;
+    case IDDHTLIB_ERROR_DATA_TIMEOUT: 
+        Serial.println("Erro\n\r\tData time out erro"); 
+        break;
+    case IDDHTLIB_ERROR_ACQUIRING: 
+        Serial.println("Erro\n\r\tAcquiring"); 
+        break;
+    case IDDHTLIB_ERROR_DELTA: 
+        Serial.println("Erro\n\r\tDelta time to small"); 
+        break;
+    case IDDHTLIB_ERROR_NOTSTARTED: 
+        Serial.println("Erro\n\r\tNao iniciado"); 
+        break;
+    default: 
+        Serial.println("Erro Desconhecido"); 
+        break;
+    }
+    
+    float valor = DHT11.getCelsius();
+     
+    if (!isnan(valor)) {             
+      temperaturaC = valor;
+    }
 
-  Serial.print("Temperature (K): ");
-  Serial.println(DHT11.getKelvin(), 2);
+    valor = DHT11.getHumidity();
+    if (!isnan(valor)) {      
+      umidade = valor;
+    }  
 
-  Serial.print("Dew Point (oC): ");
-  Serial.println(DHT11.getDewPoint());
+    valor = DHT11.getFahrenheit();
+    if (!isnan(valor)) {      
+      temperaturaF = valor;
+    }  
 
-  Serial.print("Dew Point Slow (oC): ");
-  Serial.println(DHT11.getDewPointSlow());
+    valor = DHT11.getKelvin();
+    if (!isnan(valor)) {      
+      temperaturaK = valor;
+    }  
 
-  delay(2000);
+    valor = DHT11.getDewPoint();
+    if (!isnan(valor)) {      
+      dewPoint = valor;
+    }  
+   
+    valor = DHT11.getDewPointSlow();
+    if (!isnan(valor)) {      
+      dewPointSlow = valor;
+    } 
+
+    delayLeitura = millis();     
+  }
 }
 
